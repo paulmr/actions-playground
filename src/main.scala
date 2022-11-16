3130991aa7e698dbdfeb205dc4d9ab45ee841376
Paul Roberts <paul.roberts@guardian.co.uk>
2022-11-16 16:53:48 +0000

    summary

--- src/main.scala
+++ src/main.scala
@@ -1,5 +1,13 @@
 object Example {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+    val summary = {
+      val fname = Option(System.getenv("GITHUB_STEP_SUMMARY")).getOrElse("summary-dev.txt")
+      new java.io.PrintWriter(
+        new java.io.FileOutputStream(fname, true))
+    }
+
+    summary.println("## Hello World")
+
+    summary.close()
   }
 }
