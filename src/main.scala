4f03411d44b0f197fa5eec15e7aef279fd99afb2
Paul Roberts <paul.roberts@guardian.co.uk>
2022-11-17 12:39:20 +0000

    make it do capi stuff

--- src/main.scala
+++ src/main.scala
@@ -1,4 +1,23 @@
-object Example {
+//> using lib "com.lihaoyi::requests:0.7.1"
+//> using lib "org.scala-lang.modules::scala-xml:2.1.0"
+
+import scala.xml.XML
+import scala.concurrent.duration._
+
+case class RequestStat(bodyLength: Long, valid: Boolean, duration: Duration) {
+  val tableLine = {
+    val validStr =
+      if(valid)
+        """<img src="https://github.githubassets.com/images/icons/emoji/unicode/2714.png" height="20"/>"""
+      else
+        """<img src="https://github.githubassets.com/images/icons/emoji/unicode/274c.png" height="20"/>"""
+    // f"Body length: ${bodyLength}%10d; valid: ${validStr}%5s; duration: ${duration.toMillis}ms"
+    f"|${bodyLength}%10d|${validStr}%5s|${duration.toMillis}ms|"
+  }
+}
+
+object ContentApiBodyLength {
+
   def main(args: Array[String]): Unit = {
     val summary = {
       val fname = Option(System.getenv("GITHUB_STEP_SUMMARY")).getOrElse("summary-dev.txt")
@@ -6,11 +25,65 @@ object Example {
         new java.io.FileOutputStream(fname, true))
     }
 
-    val secret = Option(System.getenv("GHA_TEST_SECRET")).getOrElse("?missing?")
+    val url = "https://content.guardianapis.com/search"
+    val apiKey = Option(System.getenv("CAPI_KEY")).get
 
-    summary.println("## Hello World")
+    val params = Map(
+      "format"        -> "xml",
+      "show-elements" -> "image",
+      "order-by"      -> "newest",
+      "show-fields"   -> "all",
+      "page-size"     -> "200",
+      "page"          -> "3",
+      "from-date"     -> "2022-11-13T06:59:44Z",
+      "to-date"       -> "2022-11-15T06:59:44Z",
+      "api-key"       -> apiKey,
+      "use-date"      -> "published"
+    )
 
-    summary.println(s"Secret from env: $secret")
+
+    val sess = requests.Session(
+      headers = requests.BaseSession.defaultHeaders - "Accept-Encoding" // disable gzip compression
+    )
+
+    val max = 2
+    val delay = 20.millis
+
+    println(s"Running $max requests")
+
+    val statsBuf = scala.collection.mutable.ListBuffer.empty[RequestStat]
+
+    for(n <- (0 until max)) {
+      val start = System.nanoTime()
+      val res = sess.get(
+        url, params=params, compress=requests.Compress.None
+      )
+      val dur = Duration.fromNanos(System.nanoTime() - start)
+
+      val validXml = try {
+        XML.loadString(res.text())
+        true
+      } catch {
+        case e: Throwable => false
+      }
+
+      val stat = RequestStat(res.bytes.length, validXml, dur)
+
+      statsBuf += stat
+
+      Thread.sleep(delay.toMillis)
+    }
+
+    val stats = statsBuf.toList
+
+    val validCount = stats.filter(_.valid).length
+    val sizes = stats.map(_.bodyLength).toSet
+
+    summary.println("## Test result")
+    summary.println(s"Valid: $validCount of $max")
+
+    summary.println("\n|Body len|Valid|Duration|")
+    for(stat <- stats) summary.println(stat.tableLine)
 
     summary.close()
   }
